7d197d290cfe3ac79935f63258e0c16b8835bc78
tetr4lab <48040768+tetr4lab@users.noreply.github.com>
2022-04-09 15:29:16 +0900

    GUIクラスの分離

--- Assets/GetSharedData/Editor/GetSharedData.cs
+++ Assets/GetSharedData/Editor/GetSharedData.cs
@@ -11,10 +11,111 @@ using Tetr4lab;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
-public sealed class GetSharedData : EditorWindow, IPreprocessBuildWithReport {
+public sealed class GetSharedData : IPreprocessBuildWithReport {
 
-	/// <summary>ビルド前に最優先で実行</summary>
-	public int callbackOrder { get { return 0; } }
+	/// <summary>エディタウインドウ</summary>
+	private class GetSharedDataWindow : EditorWindow {
+
+		/// <summary>ウインドウを開く</summary>
+		[MenuItem ("Window/GetSharedData/Open Setting Window")]
+		private static void Open () {
+			SingletonObject = GetWindow<GetSharedDataWindow> ();
+			var icon = AssetDatabase.LoadAssetAtPath<Texture> (WindowIconPath);
+			SingletonObject.titleContent = new GUIContent ("GetSharedData", icon);
+			Debug.Log ($"GetSharedData: Project {PlayerSettings.companyName}/{PlayerSettings.productName}");
+		}
+
+		/// <summary>破棄</summary>
+		private void OnDestroy () => SingletonObject = null;
+
+		/// <summary>有効化</summary>
+		private void OnEnable () => LoadPrefs ();
+
+		/// <summary>無効化</summary>
+		private void OnDisable () => SavePrefs ();
+
+		/// <summary>表示</summary>
+		private void OnGUI () {
+			EditorGUILayout.LabelField ("Project Settings", EditorStyles.boldLabel);
+			EditorGUILayout.BeginHorizontal ();
+			GUILayout.Space (10f);
+			EditorGUILayout.BeginVertical ();
+			// document
+			Document = EditorGUILayout.TextField (new GUIContent ("Document ID", "the ID of Google Spreadsheet"), Document);
+			// asset path
+			AssetPath = EditorGUILayout.TextField (new GUIContent ("Asset Folder", "the path of assets to create"), AssetPath);
+			EditorGUILayout.EndVertical ();
+			EditorGUILayout.EndHorizontal ();
+
+			// get button
+			GUILayout.Space (20f);
+			EditorGUILayout.BeginHorizontal ();
+			GUILayout.Space (10f);
+			EditorGUI.BeginDisabledGroup (EditorApplication.isCompiling || Translator != null || string.IsNullOrEmpty (Application) || string.IsNullOrEmpty (Document) || string.IsNullOrEmpty (AssetPath));
+			if (GUILayout.Button ("GetSharedData", GUILayout.Height (30))) { onGetData (); }
+			EditorGUI.EndDisabledGroup ();
+			GUILayout.Space (40f);
+			// abort button
+			EditorGUI.BeginDisabledGroup (EditorApplication.isCompiling || Translator == null || TokenSource.Token.IsCancellationRequested);
+			if (GUILayout.Button ("Abort", GUILayout.Height (30), GUILayout.Width (100))) { onAbort (); }
+			EditorGUI.EndDisabledGroup ();
+			GUILayout.Space (10f);
+			EditorGUILayout.EndHorizontal ();
+			GUILayout.Space (20f);
+
+			// build number
+			BuildNumberSettingsIsOpen = EditorGUILayout.Foldout (BuildNumberSettingsIsOpen, "Build/Bundle Number Settings", false);
+			if (BuildNumberSettingsIsOpen) {
+				EditorGUILayout.BeginHorizontal ();
+				GUILayout.Space (20f);
+				EditorGUILayout.BeginVertical ();
+				UnifiedNumber = EditorGUILayout.Toggle (new GUIContent ("Unified", "give all targets the maximum build number"), UnifiedNumber);
+				AutoIncrement = EditorGUILayout.Toggle (new GUIContent ("Auto Increment", "automatically increment build number after build"), AutoIncrement);
+				EditorGUILayout.EndVertical ();
+				EditorGUILayout.EndHorizontal ();
+			}
+			GUILayout.Space (10f);
+
+			// oauth
+			OAuthSettingsIsOpen = EditorGUILayout.Foldout (OAuthSettingsIsOpen, "OAuth Settings", false);
+			if (OAuthSettingsIsOpen) {
+				EditorGUILayout.BeginHorizontal ();
+				GUILayout.Space (10f);
+				EditorGUILayout.BeginVertical ();
+				// application
+				Application = EditorGUILayout.TextField (new GUIContent ("Application URL", "the URL of Google Apps Script"), Application);
+				// client id
+				ClientId = EditorGUILayout.TextField (new GUIContent ("Client ID", "the ID for Google Apps Script"), ClientId);
+				// client secret
+				ClientSecret = EditorGUILayout.TextField (new GUIContent ("Client Secret", "the secret for Google Apps Script"), ClientSecret);
+				// tokens
+				EditorGUILayout.BeginHorizontal ();
+				EditorGUILayout.BeginVertical ();
+				// access token
+				EditorGUI.BeginDisabledGroup (true);
+				EditorGUILayout.TextField (new GUIContent ("Access Token"), AccessToken);
+				EditorGUI.EndDisabledGroup ();
+				// refresh token
+				EditorGUI.BeginDisabledGroup (true);
+				EditorGUILayout.TextField (new GUIContent ("Refresh Token"), RefreshToken);
+				EditorGUI.EndDisabledGroup ();
+				EditorGUILayout.EndVertical ();
+				EditorGUI.BeginDisabledGroup ((string.IsNullOrEmpty (AccessToken) && string.IsNullOrEmpty (RefreshToken)) || EditorApplication.isCompiling || Translator != null);
+				if (GUILayout.Button ("Clear", GUILayout.Height (30), GUILayout.Width (60))) { AccessToken = RefreshToken = ""; OAuth.ClearTokens (); }
+				EditorGUI.EndDisabledGroup ();
+				EditorGUILayout.EndHorizontal ();
+				// guid
+				EditorGUILayout.HelpBox ("Shared settings between projects.", MessageType.None);
+				// 
+				EditorGUILayout.EndVertical ();
+				EditorGUILayout.EndHorizontal ();
+			}
+		}
+
+    }
+
+    /// <summary>ビルド前に最優先で実行</summary>
+    public int callbackOrder { get { return 0; } }
 
 	#region Static
 
@@ -74,7 +175,7 @@ public sealed class GetSharedData : EditorWindow, IPreprocessBuildWithReport {
 	private static bool BuildNumberSettingsIsOpen = false;
 
 	/// <summary>ウィンドウオブジェクト</summary>
-	private static GetSharedData SingletonObject;
+	private static GetSharedDataWindow SingletonObject;
 
 	/// <summary>中断トークン</summary>
 	private static CancellationTokenSource TokenSource;
@@ -132,15 +233,6 @@ public sealed class GetSharedData : EditorWindow, IPreprocessBuildWithReport {
 			TokenSource.Cancel ();
 			Debug.LogWarning ("GetSharedData: cancel-request by user");
 		}
-	}
-
-	/// <summary>ウインドウを開く</summary>
-	[MenuItem ("Window/GetSharedData/Open Setting Window")]
-	private static void Open () {
-		SingletonObject = GetWindow<GetSharedData> ();
-		var icon = AssetDatabase.LoadAssetAtPath<Texture> (WindowIconPath);
-		SingletonObject.titleContent = new GUIContent ("GetSharedData", icon);
-		Debug.Log ($"GetSharedData: Project {PlayerSettings.companyName}/{PlayerSettings.productName}");
 	}
 
 	/// <summary>非同期呼び出しと終了時の処理</summary>
@@ -225,93 +317,6 @@ public sealed class GetSharedData : EditorWindow, IPreprocessBuildWithReport {
 	#endregion
 
 	#region EventHandler
-
-	/// <summary>破棄</summary>
-	private void OnDestroy () => SingletonObject = null;
-
-	/// <summary>有効化</summary>
-	private void OnEnable () => LoadPrefs ();
-	
-	/// <summary>無効化</summary>
-	private void OnDisable () => SavePrefs ();
-
-	/// <summary>表示</summary>
-	private void OnGUI () {
-		EditorGUILayout.LabelField ("Project Settings", EditorStyles.boldLabel);
-		EditorGUILayout.BeginHorizontal ();
-		GUILayout.Space (10f);
-		EditorGUILayout.BeginVertical ();
-		// document
-		Document = EditorGUILayout.TextField (new GUIContent ("Document ID", "the ID of Google Spreadsheet"), Document);
-		// asset path
-		AssetPath = EditorGUILayout.TextField (new GUIContent ("Asset Folder", "the path of assets to create"), AssetPath);
-		EditorGUILayout.EndVertical ();
-		EditorGUILayout.EndHorizontal ();
-
-		// get button
-		GUILayout.Space (20f);
-		EditorGUILayout.BeginHorizontal ();
-		GUILayout.Space (10f);
-		EditorGUI.BeginDisabledGroup (EditorApplication.isCompiling || Translator != null || string.IsNullOrEmpty (Application) || string.IsNullOrEmpty (Document) || string.IsNullOrEmpty (AssetPath));
-		if (GUILayout.Button ("GetSharedData", GUILayout.Height (30))) { onGetData (); }
-		EditorGUI.EndDisabledGroup ();
-		GUILayout.Space (40f);
-		// abort button
-		EditorGUI.BeginDisabledGroup (EditorApplication.isCompiling || Translator == null || TokenSource.Token.IsCancellationRequested);
-		if (GUILayout.Button ("Abort", GUILayout.Height (30), GUILayout.Width (100))) { onAbort (); }
-		EditorGUI.EndDisabledGroup ();
-		GUILayout.Space (10f);
-		EditorGUILayout.EndHorizontal ();
-		GUILayout.Space (20f);
-
-		// build number
-		BuildNumberSettingsIsOpen = EditorGUILayout.Foldout (BuildNumberSettingsIsOpen, "Build/Bundle Number Settings", false);
-		if (BuildNumberSettingsIsOpen) {
-			EditorGUILayout.BeginHorizontal ();
-			GUILayout.Space (20f);
-			EditorGUILayout.BeginVertical ();
-			UnifiedNumber = EditorGUILayout.Toggle (new GUIContent ("Unified", "give all targets the maximum build number"), UnifiedNumber);
-			AutoIncrement = EditorGUILayout.Toggle (new GUIContent ("Auto Increment", "automatically increment build number after build"), AutoIncrement);
-			EditorGUILayout.EndVertical ();
-			EditorGUILayout.EndHorizontal ();
-		}
-		GUILayout.Space (10f);
-
-		// oauth
-		OAuthSettingsIsOpen = EditorGUILayout.Foldout (OAuthSettingsIsOpen, "OAuth Settings", false);
-		if (OAuthSettingsIsOpen) {
-			EditorGUILayout.BeginHorizontal ();
-			GUILayout.Space (10f);
-			EditorGUILayout.BeginVertical ();
-			// application
-			Application = EditorGUILayout.TextField (new GUIContent ("Application URL", "the URL of Google Apps Script"), Application);
-			// client id
-			ClientId = EditorGUILayout.TextField (new GUIContent ("Client ID", "the ID for Google Apps Script"), ClientId);
-			// client secret
-			ClientSecret = EditorGUILayout.TextField (new GUIContent ("Client Secret", "the secret for Google Apps Script"), ClientSecret);
-			// tokens
-			EditorGUILayout.BeginHorizontal ();
-			EditorGUILayout.BeginVertical ();
-			// access token
-			EditorGUI.BeginDisabledGroup (true);
-			EditorGUILayout.TextField (new GUIContent ("Access Token"), AccessToken);
-			EditorGUI.EndDisabledGroup ();
-			// refresh token
-			EditorGUI.BeginDisabledGroup (true);
-			EditorGUILayout.TextField (new GUIContent ("Refresh Token"), RefreshToken);
-			EditorGUI.EndDisabledGroup ();
-			EditorGUILayout.EndVertical ();
-			EditorGUI.BeginDisabledGroup ((string.IsNullOrEmpty (AccessToken) && string.IsNullOrEmpty (RefreshToken)) || EditorApplication.isCompiling || Translator != null);
-			if (GUILayout.Button ("Clear", GUILayout.Height (30), GUILayout.Width (60))) { AccessToken = RefreshToken = ""; OAuth.ClearTokens (); }
-			EditorGUI.EndDisabledGroup ();
-			EditorGUILayout.EndHorizontal ();
-			// guid
-			EditorGUILayout.HelpBox ("Shared settings between projects.", MessageType.None);
-			// 
-			EditorGUILayout.EndVertical ();
-			EditorGUILayout.EndHorizontal ();
-		}
-	}
 
 	/// <summary>ビルド前に実行</summary>
 	public void OnPreprocessBuild (BuildReport report) => SavePrefs ();
